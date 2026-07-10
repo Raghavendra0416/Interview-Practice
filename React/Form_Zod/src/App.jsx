1e6bdbedd8992edab8114babfecb592a57a598ab
Raghavendra0416 <raghavendra.workin@gmail.com>
2026-07-10 15:49:01 +0530

    Basic Form

--- React/Form_Zod/src/App.jsx
+++ React/Form_Zod/src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css'
-
+import BasicForm from './Components/Basic_Form/BasicForm';
+import ComplexForm from './Components/Complex_Form/ComplexForm'
 function App() {
-
   return (
     <>
+      <BasicForm />
     </>
   )
 }
---
+++ React/Form_Zod/src/Components/Complex_Form/ComplexForm.jsx
@@ -0,0 +1,9 @@
+import "./form.css"
+
+function ComplexForm() {
+    return <>
+        <h1>User Information</h1>
+    </>
+}
+
+export default ComplexForm;
