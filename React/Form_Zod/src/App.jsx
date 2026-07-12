5b06a832800fcd3e772dc52c2d1bb614ae002d4c
Raghavendra0416 <raghavendra.workin@gmail.com>
2026-07-12 20:05:32 +0530

    Practice Form

--- React/Form_Zod/src/App.jsx
+++ React/Form_Zod/src/App.jsx
@@ -1,5 +1,4 @@
 import './App.css'
-import BasicForm from './Components/Basic_Form/BasicForm';
 import ComplexForm from './Components/Complex_Form/ComplexForm';
 
 import Login from "./Components/Login/Login";
